fb160b8afb5d6d7c337c06d74dc16c27a6c3066e
Cojcoaru Claudiu-Alexandru <alexcojocaru02@gmail.com>
2023-05-25 16:05:17 +0300

    Create api

---
+++ ShoppingAssistant.Api/ShoppingAssistant.Api/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingAssistant.Api.Models;
+using ShoppingAssistant.Api.Services;
+
+namespace ShoppingAssistant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        public ProductController() { }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetProduct()
+        {
+            var productService = new ProductService();
+
+            return await productService.GetAllProductsAsync();
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var productService = new ProductService();
+
+            var product = productService.GetProductAsync(id);
+
+            return product == null ? NotFound() : Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            var productService = new ProductService();
+
+            productService.AddProduct(product);
+
+            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+        }
+    }
+}
---
+++ ShoppingAssistant.Api/ShoppingAssistant.Api/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ShoppingAssistant.Api.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Barcode { get; set; }
+
+        public string Description { get; set; }
+
+        public ProductCategory Category { get; set; }
+
+        public double Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public int StoreId { get; set; }
+
+        public List<double> PriceHistory { get; set; }
+    }
+
+    public enum ProductCategory
+    {
+        Electronics,
+        Clothing,
+        Beauty,
+        Food,
+        Home,
+        Sports
+    }
+}
